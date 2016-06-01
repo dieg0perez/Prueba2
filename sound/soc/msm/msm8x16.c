4d864a367e744b5523bdd6a75e73ff8e09805136
umang96 <umangleekha3@gmail.com>
2016-06-01 17:04:25 +0530

    Revert "ASoC: msm: Remove per-device headset button thresholds"
    This reverts commit d11ae52b09eef568ca0aa417e8653035390f7fdd.

--- sound/soc/msm/msm8x16.c
+++ sound/soc/msm/msm8x16.c
@@ -1847,6 +1847,29 @@ static void *def_msm8x16_wcd_mbhc_cal(void)
 	 * 210-290 == Button 2
 	 * 360-680 == Button 3
 	 */
+#ifdef CONFIG_MACH_JALEBI
+	btn_low[0] = 0;
+	btn_high[0] = 150;
+	btn_low[1] = 150;
+	btn_high[1] = 150;
+	btn_low[2] = 150;
+	btn_high[2] = 150;
+	btn_low[3] = 150;
+	btn_high[3] = 150;
+	btn_low[4] = 150;
+	btn_high[4] = 150;
+#elif defined(CONFIG_MACH_CP8675)
+	btn_low[0] = 50;
+	btn_high[0] = 50;
+	btn_low[1] = 75;
+	btn_high[1] = 75;
+	btn_low[2] = 87;
+	btn_high[2] = 87;
+	btn_low[3] = 112;
+	btn_high[3] = 112;
+	btn_low[4] = 137;
+	btn_high[4] = 137;
+#else
 	btn_low[0] = 75;
 	btn_high[0] = 75;
 	btn_low[1] = 150;
@@ -1857,6 +1880,7 @@ static void *def_msm8x16_wcd_mbhc_cal(void)
 	btn_high[3] = 450;
 	btn_low[4] = 500;
 	btn_high[4] = 500;
+#endif
 
 	return msm8x16_wcd_cal;
 }
